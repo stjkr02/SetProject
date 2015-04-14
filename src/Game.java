eea9eb18513ed2c220194377150644d5cd6441c5
Josh Russett <stjkr02@moravian.edu>
2015-04-14 12:56:03 -0400

    Added the sekleton for the Game class and its corresponding test

---
+++ src/Game.java
@@ -0,0 +1,35 @@
+public class Game {
+  private Table t;
+  private Deck d;
+  
+  public Game() {
+  
+  }
+  
+  public Game(String givenString) {
+    
+  }
+  
+  
+  public int numSets() {
+    
+    return 0;
+  }
+  
+  
+  public int numCards() {
+    
+    return 0;
+  }
+  
+  
+  public void playRound() {
+    
+  }
+  
+  
+  public boolean isGameOver() {
+    
+    return false;
+  }
+}
